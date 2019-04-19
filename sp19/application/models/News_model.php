282724d59445c5eb146ca75a7dfdbf52f0e97a7a
Bill Newman <william.newman@seattlecolleges.edu>
2019-04-18 17:23:28 -0700

    Adds feedback and slug to Create()
    Now when a a new item is added, the slug is passed to the view and we see the new item, with feedback!

--- sp19/application/models/News_model.php
+++ sp19/application/models/News_model.php
@@ -33,8 +33,13 @@ class News_model extends CI_Model {
             'text' => $this->input->post('text')
         );
 
-        return $this->db->insert('sp19_news', $data);
+        //return $this->db->insert('sp19_news', $data);
+        
+        if($this->db->insert('sp19_news', $data))
+        {//return slug - send to view page
+            return $slug;
+        }else{//return false
+            return false;
+        }
     }
-    
-
 }
